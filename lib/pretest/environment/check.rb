4c8e2a305209dd7677ce8321d330efe679b33694
Lucas Machado <lucas.dpmachado@gmail.com>
2017-03-11 02:59:23 -0300

    Add new environment variables creator to iOS/Android cases * Add new .pretest file * Add new commands to CLI

--- lib/pretest/environment/check.rb
+++ lib/pretest/environment/check.rb
@@ -117,6 +117,38 @@ module Pretest
           system 'brew install phantomjs'
           system 'brew install chromedriver'
           system 'xcode-select --install'
+          mac_gecko_installer
+        end
+
+        def mac_gecko_installer
+          system 'curl -OL https://github.com/mozilla/geckodriver/releases/download/v0.14.0/geckodriver-v0.14.0-macos.tar.gz'
+          system 'sudo tar xvjf geckodriver-v0.14.0-macos.tar.gz'
+          system 'sudo chmod +x geckodriver'
+          system 'sudo mv -f geckodriver /usr/local/share/geckodriver'
+          system 'sudo rm -rf geckodriver-v0.14.0-macos.tar.gz'
+          check_gecko_environment_variables
+        end
+
+        def check_gecko_environment_variables
+          set_gecko_env_variables
+          File.new("#{ENV['HOME']}/.pretest", 'w') unless Dir.entries(ENV['HOME']).include?('.pretest')
+          if File.read("#{ENV['HOME']}/.pretest").include?('GECKO_DRIVER')
+            puts 'GeckoDriver Home is already defined'
+          else
+            File.open("#{ENV['HOME']}/.pretest", 'a') { |file| file << @gecko_driver }
+            File.open("#{ENV['HOME']}/.bash_profile", 'a') { |file| file << @set_source }
+            system 'source ~/.bash_profile'
+            puts 'GeckoDriver defined with success'
+          end
+          system 'source ~/.pretest'
+        end
+
+        def set_gecko_env_variables
+          @gecko_driver = "\n\n# Set Gecko Driver Path
+export GECKO_DRIVER='/usr/local/share'
+export PATH='${PATH}:${GECKO_DRIVER}/geckodriver'
+export PATH='${PATH}:${GECKO_DRIVER}'".tr!("'", '"')
+          @set_source = "\n[[ -s '$HOME/.pretest'  ]] && source '$HOME/.pretest' # Load environment variables defined in pretest".tr!("'", '"')
         end
 
         def set_linux_chromedriver
@@ -127,6 +159,14 @@ module Pretest
           system 'sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver'
           system 'sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver'
           system "sudo rm -rf chromedriver_linux#{@bits}.zip"
+        end
+
+        def set_linux_geckodriver
+          system 'sudo wget -N https://github.com/mozilla/geckodriver/releases/download/v0.15.0/geckodriver-v0.15.0-linux64.tar.gz'
+          system 'sudo tar xvjf geckodriver-v0.15.0-linux64.tar.gz'
+          system 'sudo mv -f geckodriver /usr/local/share/geckodriver'
+          system 'sudo ln -s /usr/local/share/geckodriver /usr/local/bin/geckodriver'
+          system 'sudo rm -rf geckodriver-v0.15.0-linux64.tar.gz'
         end
 
         def set_linux_phantomjs
