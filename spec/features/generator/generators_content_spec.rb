65de4624be7d9903b0f7e020c7e788af3e3d8ba5
Lucas Machado <lucas.dpmachado@gmail.com>
2018-01-02 20:34:44 -0200

    Update generators content spec with new content validators

--- spec/features/generator/generators_content_spec.rb
+++ spec/features/generator/generators_content_spec.rb
@@ -8,185 +8,81 @@ describe Pretest do
     Dir.chdir(@dir)
   end
 
-  dirs = -> (dir) { Dir.entries(dir) }
+  content = -> (file) { File.read(file).strip }
 
   after(:each) do
     FileUtils.rm_rf(@dir)
     Dir.chdir(@current_dir)
   end
 
-  it 'creates a new empty project and check his fileTree' do
+  it 'creates a new empty project and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder}"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
+    feature_file    = "#{folder}/features/example.feature"
+    steps_content   = "#{folder}/features/step_definitions/step_definitions.rb"
+    env_content     = "#{folder}/features/support/env.rb"
+    hooks_content   = "#{folder}/features/support/hooks.rb"
+    pages_content   = "#{folder}/features/support/pages/example.rb"
 
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
+    expect(content.(feature_file)).to   be_empty
+    expect(content.(steps_content)).to  be_empty
+    expect(content.(env_content)).to    be_empty
+    expect(content.(hooks_content)).to  be_empty
+    expect(content.(pages_content)).to  be_empty
 
-    mobile_dir   = %w(app_installation_hook.rb app_life_cycle_hooks.rb
-                      dry_run.rb first_launch.rb)
-
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(pages)).to            include(*pages_dir)
-
-    expect(dirs.(support)).not_to      include(*mobile_dir)
+    expect('cucumber.yml').not_to       be_empty
+    expect('Gemfile').not_to            be_empty
   end
 
-  it 'creates a new project with --web_scaffold flag and check his fileTree' do
+  it 'creates a new project with --web_scaffold flag and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder} --web_scaffold"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
+    feature_file    = "#{folder}/features/example.feature"
+    steps_content   = "#{folder}/features/step_definitions/step_definitions.rb"
+    env_content     = "#{folder}/features/support/env.rb"
+    hooks_content   = "#{folder}/features/support/hooks.rb"
+    pages_content   = "#{folder}/features/support/pages/example.rb"
 
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
+    feature_data = ["Background", "Given I am on", "Feature"]
+    steps_data   = ["# Visit web page", "# Click on item"]
+    env_data     = ["if ENV['chrome']", "require 'capybara/cucumber'"]
+    hooks_data   = ["require 'selenium-webdriver'"]
+    pages_data   = ["class Example", "include Capybara::DSL"]
 
-    mobile_dir   = %w(app_installation_hook.rb app_life_cycle_hooks.rb
-                      dry_run.rb first_launch.rb)
+    expect(content.(feature_file)).to   include(*feature_data)
+    expect(content.(steps_content)).to  include(*steps_data)
+    expect(content.(env_content)).to    include(*env_data)
+    expect(content.(hooks_content)).to  include(*hooks_data)
+    expect(content.(pages_content)).to  include(*pages_data)
 
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(pages)).to            include(*pages_dir)
-
-    expect(dirs.(support)).not_to      include(*mobile_dir)
+    expect('cucumber.yml').not_to       be_empty
+    expect('Gemfile').not_to            be_empty
   end
 
   it 'create a new project with --api flag and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder} --api"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
-
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
-
-    mobile_dir   = %w(app_installation_hook.rb app_life_cycle_hooks.rb
-                      dry_run.rb first_launch.rb)
-
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(pages)).to            include(*pages_dir)
-
-    expect(dirs.(support)).not_to      include(*mobile_dir)
   end
 
-  it 'create a new project with --api_scaffold flag and check his fileTree' do
+  it 'create a new project with --api_scaffold flag and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder} --api_scaffold"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
-
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
-
-    mobile_dir   = %w(app_installation_hook.rb app_life_cycle_hooks.rb
-                      dry_run.rb first_launch.rb)
-
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(pages)).to            include(*pages_dir)
-
-    expect(dirs.(support)).not_to      include(*mobile_dir)
   end
 
-  it 'create a new project with --ios flag and check his fileTree' do
+  it 'create a new project with --ios flag and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder} --ios"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
-
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
-
-    ios          = %w(first_launch.rb dry_run.rb)
-    android      = %w(app_installation_hook.rb app_life_cycle_hooks.rb)
-
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(support)).to          include(*ios)
-
-    expect(dirs.(support)).not_to      include(*android)
   end
 
-  it 'create a new project with --android flag and check his fileTree' do
+  it 'create a new project with --android flag and check his content' do
     folder = 'pretest_spec'
     system "pretest create #{folder} --android"
 
-    features         = "#{folder}/features"
-    step_definitions = "#{folder}/features/step_definitions"
-    support          = "#{folder}/features/support"
-    pages            = "#{folder}/features/support/pages"
-
-    root_dir     = %w(cucumber.yml data features Gemfile)
-    features_dir = %w(example.feature step_definitions support)
-    steps_dir    = %w(step_definitions.rb)
-    support_dir  = %w(env.rb hooks.rb pages)
-    pages_dir    = %w(example.rb)
-
-    ios          = %w(first_launch.rb dry_run.rb)
-    android      = %w(app_installation_hook.rb app_life_cycle_hooks.rb)
-
-    expect(dirs.(folder)).to           include(*root_dir)
-    expect(dirs.(features)).to         include(*features_dir)
-    expect(dirs.(step_definitions)).to include(*steps_dir)
-    expect(dirs.(support)).to          include(*support_dir)
-    expect(dirs.(pages)).to            include(*pages_dir)
-    expect(dirs.(support)).to          include(*android)
-
-    expect(dirs.(support)).not_to      include(*ios)
-  end
-
-  it 'creates a new empty project with invalid args' do
-    folder   = 'pretest_spec'
-    dir_name = "#{folder} name"
-
-    log, val = Open3.capture2e("pretest create #{dir_name}")
-    log = log.to_s
-
-    expect(log).to            include("ERROR")
-    expect(dirs.('.')).not_to include(dir_name)
   end
 
 end
