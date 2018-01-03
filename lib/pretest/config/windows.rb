698b8fa21e48c361656b0bf45751f78a77b286c6
Lucas Machado <lucas.dpmachado@gmail.com>
2018-01-02 22:37:21 -0200

    Add errors handlers to Linux/Mac/Windows modules * Update chromedriver and geckodriver versions

--- lib/pretest/config/windows.rb
+++ lib/pretest/config/windows.rb
@@ -53,7 +53,7 @@ module Windows
 
     FileUtils.rm_rf('C:\\env_folder\\phantomjs-2.1.1-windows.zip') if Dir.entries('C:\\env_folder').include?('phantomjs-2.1.1-windows.zip')
     FileUtils.rm_rf('C:\\env_folder\\IEDriverServer_Win32_3.4.0.zip') if Dir.entries('C:\\env_folder').include?('IEDriverServer_Win32_3.4.0.zip')
-    FileUtils.rm_rf("C:\\env_folder\\geckodriver-v0.16.1-win#{@bits}.zip") if Dir.entries('C:\\env_folder').include?("geckodriver-v0.16.1-win#{@bits}.zip")
+    FileUtils.rm_rf("C:\\env_folder\\geckodriver-v0.19.1-win#{@bits}.zip") if Dir.entries('C:\\env_folder').include?("geckodriver-v0.19.1-win#{@bits}.zip")
     FileUtils.rm_rf('C:\\env_folder\\chromedriver_win32.zip') if Dir.entries('C:\\env_folder').include?('chromedriver_win32.zip')
 
     FileUtils.rm_rf('C:\\env_folder\\chromedriver.exe') if Dir.entries('C:\\env_folder').include?('chromedriver.exe')
@@ -62,10 +62,10 @@ module Windows
     FileUtils.rm_rf('C:\\env_folder\\phantomjs-2.1.1-windows') if Dir.entries('C:\\env_folder').include?('phantomjs-2.1.1-windows')
     FileUtils.rm_rf('C:\\env_folder\\phantomjs.exe') if Dir.entries('C:\\env_folder').include?('phantomjs.exe')
 
-    zip_download('chromedriver_win32.zip', 'https://chromedriver.storage.googleapis.com/2.30/chromedriver_win32.zip') unless Dir.entries('.').include?('chromedriver_win32.zip') || Dir.entries('.').include?('chrome.exe')
+    zip_download('chromedriver_win32.zip', 'https://chromedriver.storage.googleapis.com/2.34/chromedriver_win32.zip') unless Dir.entries('.').include?('chromedriver_win32.zip') || Dir.entries('.').include?('chrome.exe')
     zip_download('phantomjs-2.1.1-windows.zip', 'https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip') unless Dir.entries('.').include?('phantomjs-2.1.1-windows.zip') || Dir.entries('.').include?('phantomjs.exe')
     zip_download('IEDriverServer_Win32_3.4.0.zip', 'http://selenium-release.storage.googleapis.com/3.4/IEDriverServer_Win32_3.4.0.zip') unless Dir.entries('.').include?('IEDriverServer_Win32_3.4.0.zip') || Dir.entries('.').include?('IEDriverServer.exe')
-    zip_download("geckodriver-v0.16.1-win#{@bits}.zip", "https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-win#{@bits}.zip") unless Dir.entries('.').include?("geckodriver-v0.16.1-win#{@bits}") || Dir.entries('.').include?('geckodriver.exe')
+    zip_download("geckodriver-v0.19.1-win#{@bits}.zip", "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-win#{@bits}.zip") unless Dir.entries('.').include?("geckodriver-v0.19.1-win#{@bits}") || Dir.entries('.').include?('geckodriver.exe')
   end
 
   def unzip_windows_files
@@ -73,7 +73,7 @@ module Windows
     unzip_file('chromedriver_win32.zip', '.') unless Dir.entries('.').include?('chromedriver.exe')
     unzip_file('phantomjs-2.1.1-windows.zip', '.') unless Dir.entries('.').include?('phantomjs.exe') || File.directory?('phantomjs-2.1.1-windows')
     unzip_file('IEDriverServer_Win32_3.4.0.zip', '.') unless Dir.entries('.').include?('IEDriverServer.exe')
-    unzip_file("geckodriver-v0.16.1-win#{@bits}.zip", '.') unless Dir.entries('.').include?('geckodriver.exe')
+    unzip_file("geckodriver-v0.19.1-win#{@bits}.zip", '.') unless Dir.entries('.').include?('geckodriver.exe')
   end
 
   # # This is going to be a new feature.
